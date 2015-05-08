fcbc850e41af58708f27c8bf9fe4882dc6b65a1d
Your Name <you@example.com>
2015-05-08 18:04:32 +0800

    lab result

--- lib/assessment.rb
+++ lib/assessment.rb
@@ -1,3 +1,46 @@
-# Write your answer bellow
-#
-# Happy coding!!
+class Person
+	
+	def initialize
+		@first_name = first_name
+		@last_name  = last_name
+		@full_name  = full_name
+	end
+
+  attr_accessor :first_name, :last_name, :full_name #if I dont do this, then I cant do line 12-13
+end 
+
+elia = Person.new
+elia.first_name = "Elia"
+
+class Player < Person 
+	def initialize
+		@health   = 20
+		@strength = 5
+		@alive    = true
+	end
+	attr_accessor :health, :strength, :alive 
+
+	def take_damage(attack_strength)
+		@health = @health - attack_strength
+	end 
+
+	def attack(player)
+		player.take_damage(self.strength)
+	end
+end 
+
+class Knight < Player
+	def initialize
+		@health = 50
+		@strength =7
+	end
+end
+
+class Wizard < Player
+	def initialize
+		@health = 20
+		@strength = 75
+	end
+end
+
+
